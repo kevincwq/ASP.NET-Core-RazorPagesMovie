244df396d53dd208dccbdfef0c4ec03cc8366160
Peter Chen <peter_chen@trimble.com>
2019-04-19 16:13:17 -0600

    Update the project to service hostable

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -1,24 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace TodoApi
 {
+    /// <summary>
+    /// Follow https://docs.microsoft.com/en-us/aspnet/core/host-and-deploy/windows-service?view=aspnetcore-2.2
+    /// Configure the project and update program to service hostable
+    /// </summary>
     public class Program
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var isService = !(Debugger.IsAttached || args.Contains("--console"));
+
+            if (isService)
+            {
+                var pathToExe = Process.GetCurrentProcess().MainModule.FileName;
+                var pathToContentRoot = Path.GetDirectoryName(pathToExe);
+                Directory.SetCurrentDirectory(pathToContentRoot);
+            }
+
+            var builder = CreateWebHostBuilder(args.Where(arg => arg != "--console").ToArray());
+            var host = builder.Build();
+
+            if (isService)
+            {
+                // To run the app without the CustomWebHostService change the
+                // next line to host.RunAsService();
+                host.RunAsCustomService();
+            }
+            else
+            {
+                host.Run();
+            }
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+            .ConfigureLogging((hostingContext, logging) =>
+            {
+                logging.AddEventLog();
+            })
+            .ConfigureAppConfiguration((context, config) =>
+            {
+                // Configure the app here.
+            })
+            .UseStartup<Startup>();
     }
 }
